9e6984444d7d3d2bb59e9ec59a52116c08d3d0eb
ahsanatzapier <ahsan.hassan@zapier.com>
2022-09-07 13:22:39 -0600

    New testing files to resolve issues with odd number of pages

--- NotifyPDF.py
+++ NotifyPDF.py
@@ -1,4 +1,3 @@
-
 from PyPDF2 import PdfReader, PdfWriter, Transformation, PdfFileWriter
 import os
 
@@ -27,9 +26,9 @@ def get_number_of_pages_needed_in_final_document(number_of_original_pages):
 
 def get_odd_even_transformations(sample_height):
     odd_page_transformation = Transformation().scale(
-        sx=0.5, sy=0.5).translate(tx=5, ty=sample_height/2)
+        sx=0.5, sy=0.5).translate(tx=0, ty=sample_height/2)
     even_page_transformation = Transformation().scale(
-        sx=0.5, sy=0.5).translate(tx=5, ty=0)
+        sx=0.5, sy=0.5).translate(tx=0, ty=0)
     return (odd_page_transformation, even_page_transformation)
 
 
@@ -45,6 +44,11 @@ def create_empty_pages_for_final_document(number_of_pages_needed, sample_width, 
 def create_new_pdf(number_of_original_pages, number_of_pages_needed, blank_pdf,
                    odd_page_transformation, even_page_transformation, writer,
                    page_number, original_file_name):
+
+    if (number_of_original_pages % 2) == 0:
+        print("Document has even pages")
+    else:
+        print("Document has odd pages")
 
     for i in range(number_of_original_pages):
 
@@ -64,25 +68,30 @@ def create_new_pdf(number_of_original_pages, number_of_pages_needed, blank_pdf,
         ###################################################
 
         print("C")
+
         odd_page.add_transformation(odd_page_transformation)
         print("D")
         even_page.add_transformation(even_page_transformation)
         print("E")
 
         blank_page.merge_page(odd_page)
-        print("odd page", page_number+i+1, "added")
+        print("odd page", page_number+i, "added")
         blank_page.merge_page(even_page)
-        print("even page", page_number+i+1+1, "added")
+        print("even page", page_number+i+1, "added")
 
         writer.add_page(blank_page)
         page_number += 1
         print("--- i = ", i, "loop ended")
 
-    with open(original_file_name+"_notes_version.pdf", "wb") as fp:
+    with open(original_file_name+"_Notes_Version.pdf", "wb") as fp:
         writer.write(fp)
 
 
-filename = "example.pdf"
+# filename = "CS50-36×27-Even.pdf"
+filename = "CS106-25x19-Odd.pdf"
+# filename = "PPG-27×21-Odd.pdf"
+
+
 filename_without_extension = filename.split(sep=".")[0]
 print(filename_without_extension)
 original_pdf = get_pdf(filename)
